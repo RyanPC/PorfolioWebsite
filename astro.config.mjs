fc85821c6efd88a533de41d4a64a38d1704d9f48
Ryan Peterson S3817175 <s3817175@student.rmit.edu.au>
2024-08-19 00:33:19 +1000

    Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ryanpc.github.io/',
-	base: 'PorfolioWebsite',
+	base: '/PorfolioWebsite',
 	// site: 'https://example.com',
 	// integrations: [mdx(), sitemap()],
 });
